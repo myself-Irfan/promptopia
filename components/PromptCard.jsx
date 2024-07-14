1ada7363ae56499680f41a8649ccccc2dfa1a095
Irfan <ahmed.1995.irfan@gmail.com>
2024-07-14 23:47:01 +0600

    Saw 2:48:00

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -4,11 +4,15 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import { useSession } from 'next-auth/react';
-import { usePathName, useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
   const [copied, setCopied] = useState("");
+  
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
@@ -52,11 +56,29 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         {post.prompt}
       </p>
       <p 
-        className='font-inter text-sm blue_gradient cursor-pointer'
+        className='font-inter text-sm green_gradient cursor-pointer'
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
         #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && 
+        pathName === '/profile' && (
+        <div className="mt-5 flex-center gap-5 border-t border-gray-100 pt-3">
+          <p 
+            className="font-inter text-sm blue_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p 
+            className="font-inter text-sm red_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
 
     </div>
   )
--- models/user.js
+++ models/user.js
@@ -20,6 +20,7 @@ const UserSchema = new Schema({
     }
 })
 
+
 const User = models.User || model("User", UserSchema);
 
 export default User;
